c4cd4a71f10ea3dc303b0629e2e5f2799f01391c
NicolasBonilla03 <inbonilla@uniboyaca.edu.co>
2024-11-22 01:09:39 -0500

    Mas arreglos menores

--- app/src/main/java/com/example/pokedex/InfoPokemon.kt
+++ app/src/main/java/com/example/pokedex/InfoPokemon.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
